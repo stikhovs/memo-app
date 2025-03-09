fc49aaa7db13798fd1c98cbf54a919a3f5efa246
Sergei Tikhov <s.tikhov@unabank.com>
2025-03-09 21:00:57 +0200

    Remove unnecessary fields from UserDto

--- src/main/java/com/sergio/memo_app/persistence/dto/UserDto.java
+++ src/main/java/com/sergio/memo_app/persistence/dto/UserDto.java
@@ -5,8 +5,6 @@ import lombok.Builder;
 @Builder(toBuilder = true)
 public record UserDto(Integer id,
                       String username,
-                      String email,
-                      String telegramUsername,
-                      String telegramChatId) {
+                      String email) {
 
 }
