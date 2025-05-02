66d98b7ddb922eab4d83116716166ca728f932f0
Sergei Tikhov <s.tikhov@unabank.com>
2025-05-02 18:47:09 +0300

    Return null if get user found nothing. Instead of bad request

--- src/main/java/com/sergio/memo_app/api/controller/telegram/TelegramUserController.java
+++ src/main/java/com/sergio/memo_app/api/controller/telegram/TelegramUserController.java
@@ -4,7 +4,6 @@ import com.sergio.memo_app.api.dto.TelegramUserApiDto;
 import com.sergio.memo_app.api.service.TelegramUserApiService;
 import com.sergio.memo_app.persistence.dto.TelegramUserDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -19,10 +18,9 @@ public class TelegramUserController {
     }
 
     @GetMapping("/telegram/user")
-    public ResponseEntity<TelegramUserApiDto> get(@RequestParam Long telegramUserId) {
+    public TelegramUserApiDto get(@RequestParam Long telegramUserId) {
         return telegramUserApiService.get(telegramUserId)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.badRequest().build());
+                .orElse(null);
     }
 
 }
